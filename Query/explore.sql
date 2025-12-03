3e95aae1f1b4eaa4086ea912405e5f6af8ff1790
Bryan Keating <ru.bryankeating@gmail.com>
2025-12-03 17:56:38 -0500

    Average plys per game

--- Query/explore.sql
+++ Query/explore.sql
@@ -4,3 +4,13 @@ FROM game_data
 WHERE Variant IS NOT NULL
 	AND ply = 1;
 
+-- Average plys per game
+SELECT 
+    AVG(max_ply) AS avg_plys,
+    MIN(max_ply) AS shortest_game,
+    MAX(max_ply) AS longest_game
+FROM (
+    SELECT game_id, MAX(ply) AS max_ply
+    FROM game_data
+    GROUP BY game_id
+);
